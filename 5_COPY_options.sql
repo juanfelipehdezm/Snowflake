9ef4d57a17851786a5e54ac1e1ced6db4b2ff8f8
juan felipe hernandez <juanfelipehdezm@example.com>
2024-07-04 11:13:38 -0500

    options using COPY

--- 5_COPY_options.sql
+++ 5_COPY_options.sql
@@ -9,7 +9,7 @@ CREATE OR REPLACE TABLE ORDERS_DB.PUBLIC.ORDERS_COPY_OPTIONS(
 
 CREATE OR REPLACE STAGE MANAGE_BD.EXTERNAL_STAGES.aws_atage_copy_options
     url = 's3://snowflakebucket-copyoption/returnfailed/';
-
+------------------------------------------------------------------------
 --VALIDATION MODE
 -- return_errors = will provide the errors in find on the files
 -- return_n_rows = will validate the first n rows
@@ -25,3 +25,15 @@ COPY INTO ORDERS_DB.PUBLIC.ORDERS_COPY_OPTIONS
     file_format = (type=csv field_delimiter=',' skip_header=1)
     pattern= '.*Order.*'
     VALIDATION_MODE = return_20_rows;
+-------------------------------------------------------------------------
+--SIZE_LIMIT
+CREATE OR REPLACE STAGE MANAGE_BD.EXTERNAL_STAGES.aws_atage_copy_options
+    url = 's3://snowflakebucket-copyoption/size/';
+
+--the size of the file are 54600 bytes each
+--the size we choose means all of the files combines.
+COPY INTO ORDERS_DB.PUBLIC.ORDERS_COPY_OPTIONS
+    FROM @MANAGE_BD.EXTERNAL_STAGES.aws_atage_copy_options
+    file_format = (type = csv field_delimiter = ',' skip_header = 1)
+    pattern = '.*Order.*'
+    SIZE_LIMIT = 20000;
