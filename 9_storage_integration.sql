4111420fab7397ba50416566ed7d372207962979
juan felipe hernandez <juanfelipehdezm@example.com>
2024-10-03 19:55:34 -0500

    better description for storage integration

--- 9_storage_integration.sql
+++ 9_storage_integration.sql
@@ -1,11 +1,33 @@
+USE ROLE ACCOUNTADMIN; 
+USE WAREHOUSE COMPUTE_WH;
+USE DATABASE CERTIFICATION_TEST_DB;
+USE SCHEMA TEST;
+
+--
+-- before creating we must first create a role on s3 and grant permission to s3 bucket
+-- and use the arn as part of the creating ot the storage integration object
+--
 --create storage integratin object
 CREATE OR REPLACE STORAGE INTEGRATION S3_AWS
     TYPE = EXTERNAL_STAGE
     STORAGE_PROVIDER = S3
     ENABLED = TRUE
-    STORAGE_AWS_ROLE_ARN = ''
+    STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::083527689950:role/juanhernandezm'
     STORAGE_ALLOWED_LOCATIONS = ('s3://snow-flake-demoproject/csv/','s3://snow-flake-demoproject/json/')
         COMMENT = 'Storage integration for s3 path';
 
----see storege integration
+
 DESC INTEGRATION S3_AWS;
+-- as result of the DESC we copy the information from STORAGE_AWS_IAM_USER_ARN and
+-- add it to a trusted relathionship
+
+
+--now we create the stage
+CREATE OR REPLACE stage S3_MOVIE
+    URL = 's3://snow-flake-demoproject/csv/'
+    STORAGE_INTEGRATION = S3_AWS
+    FILE_FORMAT = CSV_FILE_FORMAT;
+
+
+--- and we can see the file inside the s3 bucket
+LIST @S3_MOVIE;
